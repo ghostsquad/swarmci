bb74efc97d36ceac5552a9a88c12f636328ebb40
Wes McNamee <ghost.squadron@gmail.com>
2016-07-24 18:02:20 -0700

    fixing log alignment

--- swarmci/__main__.py
+++ swarmci/__main__.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         logging.basicConfig(
             stream=sys.stdout,
             level=logging.INFO,
-            format="%(asctime)s (%10(threadName)s) %(message)s")
+            format="%(asctime)s (%(threadName)-10gs) %(message)s")
         main(sys.argv[1:])
     except Exception as e:
         traceback.print_exc()
