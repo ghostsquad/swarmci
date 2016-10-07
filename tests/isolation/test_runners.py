ee3d6d53f55eb547f2450776beb20afb691ee735
Wes McNamee <ghost.squadron@gmail.com>
2016-10-07 15:19:51 -0700

    keeping dry with tests the "run_all_container_behavior" describe should be renamed once this bug is fixed: https://github.com/ropez/pytest-describe/issues/14

--- tests/isolation/test_runners.py
+++ tests/isolation/test_runners.py
@@ -100,7 +100,7 @@ def describe_threaded_runner():
     def runner_fixture():
         return ThreadedRunner()
 
-    def describe_run_all():
+    def describe_run_all_():
         def given_single_task():
             def when_task_fails():
                 def expect_return_false(runner_fixture):
@@ -143,6 +143,7 @@ def describe_threaded_runner():
                     assert_that(runner_fixture.run_all([task1_mock, task2_mock])).is_true()
 
 
+@behaves_like(a_runner, a_serial_runner)
 def describe_docker_runner():
 
     @pytest.fixture(scope='function')
@@ -153,6 +154,11 @@ def describe_docker_runner():
     def task_fixture():
         return create_autospec(Task, spec_set=True)
 
+    @pytest.fixture(scope='module')
+    def runner_fixture():
+        docker_mock = create_autospec(DockerClient, spec_set=True)
+        return DockerRunner('foo_image', docker=docker_mock)
+
     def describe_run_in_docker():
 
         def expect_command_executed_in_container(cn_fixture):
@@ -160,12 +166,7 @@ def describe_docker_runner():
             DockerRunner.run_in_docker(expected_command, cn=cn_fixture)
             cn_fixture.execute.assert_called_once_with(expected_command)
 
-    def describe_run_all():
-
-        @pytest.fixture(scope='module')
-        def runner_fixture():
-            docker_mock = create_autospec(DockerClient, spec_set=True)
-            return DockerRunner('foo_image', docker=docker_mock)
+    def describe_run_all_container_behavior():
 
         def expect_cn_passed_to_task(cn_fixture, task_fixture):
             docker_mock = create_autospec(DockerClient, spec_set=True)
@@ -176,44 +177,3 @@ def describe_docker_runner():
 
             subject.run_all([task_fixture])
             task_fixture.execute.assert_called_once_with(cn=mock.ANY)
-
-        def given_single_task():
-            def when_task_fails():
-                def expect_return_false(runner_fixture):
-                    task_mock = create_task_mock()
-                    task_mock.successful = False
-
-                    assert_that(runner_fixture.run_all([task_mock])).is_false()
-
-            def when_task_succeeds():
-                def expect_return_true(runner_fixture):
-                    task_mock = create_task_mock()
-                    task_mock.successful = True
-
-                    assert_that(runner_fixture.run_all([task_mock])).is_true()
-
-        def given_many_tasks():
-            def when_first_task_fails():
-                def expect_later_tasks_not_run_and_return_false(runner_fixture):
-                    task1_mock, task2_mock = create_task_mock(count=2)
-                    task1_mock.successful = False
-
-                    assert_that(runner_fixture.run_all([task1_mock, task2_mock])).is_false()
-                    task1_mock.execute.assert_called_once()
-                    task2_mock.execute.assert_not_called()
-
-            def when_any_task_fails():
-                def expect_return_false(runner_fixture):
-                    task1_mock, task2_mock = create_task_mock(count=2)
-                    task1_mock.successful = True
-                    task2_mock.successful = False
-
-                    assert_that(runner_fixture.run_all([task1_mock, task2_mock])).is_false()
-
-            def when_all_tasks_succeed():
-                def expect_return_true(runner_fixture):
-                    task1_mock, task2_mock = create_task_mock(count=2)
-                    task1_mock.successful = True
-                    task2_mock.successful = True
-
-                    assert_that(runner_fixture.run_all([task1_mock, task2_mock])).is_true()
