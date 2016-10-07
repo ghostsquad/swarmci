252b995bf82ccdf649e76a69823a7efc0f8ebf65
Wes McNamee <ghost.squadron@gmail.com>
2016-10-07 16:00:42 -0700

    cleaning up unused code in util

--- swarmci/util.py
+++ swarmci/util.py
@@ -1,5 +1,4 @@
 import logging
-from functools import wraps
 
 
 def get_logger(name):
@@ -9,33 +8,7 @@ def get_logger(name):
     return _logger
 
 
-def synchronized(tlockname):
-    """A decorator to place an instance based lock around a method """
-    def _synched(func):
-        @wraps(func)
-        def _synchronizer(self,*args, **kwargs):
-            tlock = self.__getattribute__(tlockname)
-            tlock.acquire()
-            try:
-                return func(self, *args, **kwargs)
-            finally:
-                tlock.release()
-        return _synchronizer
-    return _synched
-
-
 # this allows for some shorthand like
 # x = y or raise_(ValueError)
 def raise_(ex):
     raise ex
-
-
-def lazyprop(fn):
-    attr_name = '_lazy_' + fn.__name__
-
-    @property
-    def _lazyprop(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
-        return getattr(self, attr_name)
-    return _lazyprop
